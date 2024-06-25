c1482f4fcb4e473f7b8b855d7185a4d705282682
Avinash Vaidya <avinash.vaidya@sap.com>
2024-06-25 20:03:27 +0000

    Added environment variables and integration with openai api

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-### Reference - 
 
-
-### Commands - 
+### Set up steps - 
 1. Create Dev Work Space
-2. Import/Clone project from GIT - <link>
-3. Open Command Pallete
+2. Import/Clone project from GIT - https://github.tools.sap/genai-certification-2024/certification_hub.git
     1. Set Runtime  - ">Runtime: Set Default"
     2. Create Python Environment - ">Python: Create Environment" - **Select ~/.asdf-inst/shims/python3**
     3. If you have already created the Python environment. Then select interpreter - ">Python: Select Interpreter" - then select Python virtual environment. **For example - Python 3.11.9 .venv**
+3. Update your openai api key in the .env file to run the app on your local.
+4. Create .env.development file in the resources folder and add openai api key for development server in this file.
 
 4. Every time you open workspace you might have to activate the virtual environment with the help of command - **source /home/user/projects/certification_hub/.venv/bin/activate**
 
 ### Debugging - 
-1. To check the logg, use command - **cf logs certification_hub --recent**
+1. To debug on local, run the command - python3 ./src/server.py
+2. To check the logg, use command - **cf logs certification_hub --recent**
+
 
 
 ### Important files - 
@@ -23,3 +24,11 @@
 5. **.cfignore** - ".venv" is unnecessary to run cf app, so list up the directory.
 6. **.gitignore** - To ignore files from git commit
 7. **requests.http** - This file contains the sample requests to test the APIs
+
+### Initial GIT commands
+1. git init
+2. git add README.md
+3. git commit -m "first commit"
+4. git branch -M main
+5. git remote add origin https://github.tools.sap/genai-certification-2024/certification_hub.git
+6. git push -u origin main 
--- src/certification_controller.py
+++ src/certification_controller.py
@@ -1,7 +1,32 @@
+import os
 import json
+from httpx import Client
+from openai import OpenAI
 from flask import Flask, request, Blueprint, jsonify
+import openai
+from werkzeug.wrappers import response
+from dotenv import load_dotenv
 
+# Create certification controller blue print
 certification_controller_bp = Blueprint('certification_controller', __name__)
+
+# Load common environment variables from .env file
+load_dotenv('./resources/.env')
+
+environment = os.getenv('ENVIRONMENT', 'local')  # Default to 'local' if ENVIRONMENT is not set
+
+# Load  environment specific environment variables from .env file
+if environment == 'local':
+    dotenv_local_path = './resources/.env.local'
+    load_dotenv(dotenv_local_path, override=True)  
+elif environment == 'development':
+    dotenv_dev_path = './resources/.env.development'
+    load_dotenv(dotenv_dev_path, override=True) 
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+
+openai_client = OpenAI(api_key=openai_api_key)
 
 
 @certification_controller_bp.route('/uploadPdf', methods=['POST'])
@@ -9,3 +34,25 @@ def uploadPdf():
     intput_question = json.loads(request.data)
     print("Question: ", intput_question);
     return ""
+
+
+@certification_controller_bp.route('/generateAnswer', methods=['POST'])
+def generateAnswer():
+    json_payload = request.get_json()
+    prompt = json_payload['question']
+    print("input to chatgpt: ", prompt)
+    try:
+
+        response = openai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "The content is about geographies and country"},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        generateAnswer = response.choices[0].message.content
+        print("Response: ", generateAnswer)
+        return jsonify({'response': generateAnswer})
+
+    except openai.OpenAIError as e:
+        return jsonify({'error': str(e)}), 500
